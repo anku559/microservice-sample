75c21776f03a4b013b306f829f7ea3b583dc662c
anku559 <ankitsingh91@gmail.com>
2024-02-15 01:26:37 +0530

    Changes in Main

--- gateway/main.go
+++ gateway/main.go
@@ -1,50 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"gateway/helpers"
-	"gateway/utils/constants"
-	UserTypes "gateway/utils/types"
-)
-
-func init() {
-	fmt.Println("========= Inside init (main.go:10) ========= ")
-
-	userAlex := UserTypes.User{
-		FirstName:  "Alex",
-		MiddleName: "Doe",
-		LastName:   "Jr.",
-	}
-
-	fmt.Printf("%+v\n", userAlex)
-	fmt.Printf("%s\n", UserTypes.GetFullName(userAlex))
-}
-
-func init() {
-	fmt.Println("========= Inside init (main.go:23) ========= ")
-
-	alexAddress := UserTypes.UserAddress{
-		AddressLine1: "House No. 123",
-		AddressLine2: "Street ABC",
-		AddressLine3: "XYZ Road",
-		Landmark:     "Near QWERTY Tower",
-		City:         "Ujjain",
-		State:        "Madhya Pradesh",
-		Country:      "India",
-		Pincode:      "456001",
-	}
-
-	fmt.Printf("%s\n", UserTypes.GetFullAddress(alexAddress))
-}
-
 func main() {
-	fmt.Println(constants.DaysArray[0])
-	fmt.Println("========= End ========= ")
 
-	testFilePath, _ := constants.JoinPaths("..", ".seeders", "test.txt")
-	text, _ := helpers.ReadTextFile(testFilePath)
-	fmt.Println(text)
-
-	writeToFile, _ := constants.JoinPaths("..", ".seeders", "newFile.txt")
-	helpers.WriteTextFile(writeToFile, "HELLO WORLD")
 }
